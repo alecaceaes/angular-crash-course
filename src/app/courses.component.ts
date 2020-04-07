05b555c8f428014c441808ffa154780a4c942298
Alec Tecson <alec.tecson@nipa.ph>
2020-04-07 19:10:46 +0800

    Dependency Injection

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -15,4 +16,8 @@ import { Component } from '@angular/core';
 export class CoursesComponent {
     title = 'List of Courses'
     courses;
+
+    constructor(service: CoursesService) {        
+        this.courses = service.getCourses();
+    }
 }
