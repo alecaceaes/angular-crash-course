8c1209945b170359dfa1c35a53e8bb49f1d496c6
Alec Tecson <alec.tecson@nipa.ph>
2020-04-07 20:00:42 +0800

    Property Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,22 +2,16 @@ import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'courses', // <courses> "courses"
-                         // <div class="courses"> ".courses"
-                         // <div id="courses"> "#courses"
+    selector: 'courses', 
     template: `
         <h2>{{ title }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">{{ course }}</li>
-        </ul>        
+        <h2 [textContent]="title"></h2>
+        <img src="{{ imageUrl }}" />       
+        <img [src]="imageUrl" />
     `
 })
 
 export class CoursesComponent {
-    title = 'List of Courses'
-    courses;
-
-    constructor(service: CoursesService) {        
-        this.courses = service.getCourses();
-    }
+    title = 'List of Courses';
+    imageUrl = "http://lorempixel.com/400/200";
 }
