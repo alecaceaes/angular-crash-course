fda937a083fa203bb3884c34cb0cf1830696da31
Alec Tecson <alec.tecson@nipa.ph>
2020-04-08 12:44:57 +0800

    Adding Bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,16 +4,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', 
     template: `     
-        <img [src]="imageUrl" />
-        <table>
-            <tr>
-                <td [attr.colspan]="colSpan"></td>
-            </tr>
-        </table>
+        <button class="btn btn-primary">Save</button>
     `
 })
 
 export class CoursesComponent {   
-    imageUrl = "http://lorempixel.com/400/200";
-    colSpan = 2;
+
 }
