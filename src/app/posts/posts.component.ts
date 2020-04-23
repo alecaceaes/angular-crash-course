301bb6f34247d7e92e29945cf533a11c82b045e4
Alec Tecson <alec.tecson@nipa.ph>
2020-04-23 17:50:15 +0800

    Creating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent {
   posts: any[];
+  private url = "http://jsonplaceholder.typicode.com/posts";
 
-  constructor(http: HttpClient) {
-    http.get<any[]>("http://jsonplaceholder.typicode.com/posts")
+  constructor(private http: HttpClient) {
+    http.get<any[]>(this.url)
       .subscribe(response => {
         this.posts = response;
       });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value }
+
+    input.value = '';
+
+    this.http.post<any>(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.id;
+        this.posts.splice(0, 0, post);
+        console.log(response);
+      })
   }
 }
