2ae607e90d08c74bf0e9a394a2bed80096f5d7c1
Alec Tecson <alec.tecson@nipa.ph>
2020-04-23 18:34:10 +0800

    Updating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -29,4 +29,12 @@ export class PostsComponent {
         console.log(response);
       })
   }
+
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+    .subscribe(response => {
+      console.log(response)
+    })
+    //this.http.put(this.url, JSON.stringify(post))
+  }
 }
