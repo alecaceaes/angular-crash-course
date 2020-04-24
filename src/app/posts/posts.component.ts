b59f423b85705ea8ec016ff0e9f614a6f6b6b979
Alec Tecson <alec.tecson@nipa.ph>
2020-04-24 21:29:54 +0800

    Deleting Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -37,4 +37,12 @@ export class PostsComponent {
     })
     //this.http.put(this.url, JSON.stringify(post))
   }
+
+  deletePost(post) {
+    this.http.delete(this.url + "/" + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post.id);
+        this.posts.splice(index, 1);
+      })
+  }
 }
