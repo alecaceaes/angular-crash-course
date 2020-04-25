6973a50a4f2d4a2d43567a6b1c70538e58436cb7
Alec Tecson <alec.tecson@nipa.ph>
2020-04-25 22:19:25 +0800

    Extracting a Reusable Error Handling Method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -15,31 +15,40 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    return this.http.get<any[]>(this.url);
+    return this.http.get<any[]>(this.url)
+      .pipe(
+        catchError(this.handerError)
+      );
   }
 
   createPost(post) {
     return this.http.post<any>(this.url, JSON.stringify(post))
-      .pipe(catchError((error: Response) => {
-        if(error.status === 400)
-          return throwError(new BadInput(error));
-
-        return Observable.throw(new AppError(error));
-      }));
+      .pipe(
+        catchError(this.handerError)
+      );
   }
 
   updatePost(post) {
-    return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }));
+    return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .pipe(
+        catchError(this.handerError)
+      );
   }
   
   deletePost(id) {
     return this.http.delete(this.url + "/" + id)
       .pipe(
-        catchError((error: Response) => {
-        if (error.status === 404)
-          return throwError(new NotFoundError());
+        catchError(this.handerError)
+      );
+  }
 
-        return Observable.throw(new AppError(error));      
-      }));
+  private handerError(error: Response) {
+    if(error.status === 400)
+      return throwError(new BadInput(error));
+
+    if (error.status === 404)
+      return throwError(new NotFoundError());
+
+    return Observable.throw(new AppError(error));    
   }
 }
